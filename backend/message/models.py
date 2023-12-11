f06c718da17d3c824696412a3ca9b20d605b487b
AnasAli986 <60493038+DcWire@users.noreply.github.com>
2023-12-11 17:01:16 +0530

    Initialize the project

---
+++ backend/message/models.py
@@ -0,0 +1,10 @@
+import uuid 
+from django.db import models
+
+# Create your models here.
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    message = models.TextField(null=False)
+
+    def __str__(self):
+        return self.message 
