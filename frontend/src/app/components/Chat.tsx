a67fd9b83bfb1ab576b66fd5f0bc9adc28cb30eb
AnasAli986 <60493038+DcWire@users.noreply.github.com>
2023-12-30 21:00:58 +0530

    Fix chatbox at the end

--- frontend/src/app/components/Chat.tsx
+++ frontend/src/app/components/Chat.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import arrow from "../../../public/arrow.svg";
-export default function Chat() {
+export default function Chat({ sx = {} }) {
   return (
     <div className="flex flex-col w-full bg-transparent p-4">
-      <div className="appearance-none w-full p-2 text-gray-700 border border-gray-300 rounded-full focus:outline-none focus:ring focus:ring-blue-500 relative">
+      <div className="appearance-none w-full px-7 py-2 text-gray-700 border border-gray-300 rounded-full focus:outline-none focus:ring focus:ring-blue-500 relative">
         <form>
           <input
             type="text"
             placeholder="Ask me anything..."
             className="focus:outline-none w-11/12"
           />
-          <button className="right-0.5 absolute text-white bg-transparent hover:bg-blue-600 rounded-lg focus:outline-none w-6 h-6">
+          <button className="right-2 absolute text-white bg-transparent rounded-lg focus:outline-none w-6 h-6">
             <Image src={arrow} alt="Image of arrow head" />
           </button>
         </form>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Chat from "./components/Chat";
 export default function Home() {
   return (
     <div>
-      <div>
+      <div className="fixed w-2/4 bottom-0">
         <Chat />
       </div>
     </div>
