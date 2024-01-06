71aefff94c4cfe0dcbe2a20a4bab4564278f361a
AnasAli986 <60493038+DcWire@users.noreply.github.com>
2024-01-06 13:43:03 +0530

    Clear chatbox after pressing enter

--- frontend/src/app/components/Chat.tsx
+++ frontend/src/app/components/Chat.tsx
@@ -1,24 +1,29 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import arrow from "../../../public/arrow.svg";
+import { FormEvent } from "react";
 export default function Chat({ sx = {} }) {
+  const [value, setValue] = useState("");
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    setValue("");
+  };
   return (
-    <div className="flex flex-col w-full bg-transparent p-4">
+    <div className="flex flex-col w-full bg-transparent p-4 pb-0">
       <div className="appearance-none w-full px-7 py-2 text-gray-700 border border-gray-300 rounded-full focus:outline-none focus:ring focus:ring-blue-500 relative">
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type="text"
             placeholder="Ask me anything..."
             className="focus:outline-none w-11/12"
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
           />
           <button className="right-2 absolute text-white bg-transparent rounded-lg focus:outline-none w-6 h-6">
             <Image src={arrow} alt="Image of arrow head" />
           </button>
         </form>
-      </div>
-      <div className="mt-4 text-gray-500 text-sm text-center">
-        {Math.random() > 0.5
-          ? "Feeling curious, ask me anything!"
-          : "Ready to answer your questions with my best wit!"}
       </div>
     </div>
   );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,11 @@ export default function Home() {
     <div>
       <div className="fixed w-2/4 bottom-0">
         <Chat />
+        <div className="mt-4 text-gray-500 text-sm text-center pb-1">
+          {Math.random() > 0.5
+            ? "Feeling curious, ask me anything!"
+            : "Ready to answer your questions with my best wit!"}
+        </div>
       </div>
     </div>
   );
