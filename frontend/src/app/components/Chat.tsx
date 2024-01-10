1c90e0b276201e3bc5224b1606369d76e591edb9
Amaan <miramaanalikhan@gmail.com>
2024-01-10 21:39:22 +0530

    Changed font, Added code to eliminate whitespaces from submission

--- frontend/src/app/components/Chat.tsx
+++ frontend/src/app/components/Chat.tsx
@@ -13,15 +13,15 @@ const Chat = ({ onSubmit, temp, value, setValue }: ChatProps) => {
       key={"chat-main"}
     >
       <div className="appearance-none w-full px-7 py-2 text-gray-700 border border-gray-300 rounded-full focus:outline-none focus:ring focus:ring-blue-500 relative">
-        <form onSubmit={onSubmit}>
+        <form key={"chatForm"} onSubmit={onSubmit}>
           <input
             type="text"
             placeholder="Ask me anything..."
             className="focus:outline-none w-11/12"
             value={value}
-            onChange={(event) => setValue(event.target.value)}
+            onChange={(ev) => setValue(ev.target.value)}
           />
-          <button className="right-2 absolute text-white bg-transparent rounded-lg focus:outline-none w-6 h-6">
+          <button className="right-2 absolute text-white bg-transparent rounded-lg focus:outline-none w-6 h-6 cursor-pointer hover:bg-gray-200">
             <Image src={arrow} alt="Image of arrow head" />
           </button>
         </form>
--- frontend/src/app/components/ChatMessages.tsx
+++ frontend/src/app/components/ChatMessages.tsx
@@ -1,4 +1,17 @@
+import Home from "../page";
 type ChatMessagesProps = { data: Array<Object> };
 export default function ChatMessages({ data }: ChatMessagesProps) {
-  return <div></div>;
+  return (
+    <div style={{ maxHeight: "80vh" }} className="overflow-auto pt-2">
+      {data.map((text: any, index) => {
+        return (
+          <div className="w-2/4 max-w-[500px] leading-1.5 p-1 bg-indigo-500 rounded-e-xl rounded-es-xl my-5">
+            <p className="text-md font-normal text-white pl-2 break-normal">
+              {text}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
